44d645e5f566de36cba733a56260304aeb79fe40
annajulyapontes <140862312+annajulyapontes@users.noreply.github.com>
2024-05-28 22:29:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-29T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-29T00:00:00");
 
 const tempo = tempoObjetivo1 ;
 
@@ -16,14 +16,13 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
-      return [dias, horas, minutos, segundos];
+      return [, horas, minutos, segundos];
     } else {
-      return [0, 0, 0, 0];
+      return [0, 0, 0];
     }
   }
 
   function atualizaCronometro() {
-    document.getElementById("dias0").textContent = calculaTempo(tempo)[0];
     document.getElementById("horas0").textContent = calculaTempo(tempo)[1];
     document.getElementById("min0").textContent = calculaTempo(tempo)[2];
     document.getElementById("seg0").textContent = calculaTempo(tempo)[3];
